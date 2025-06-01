9ad0c15ffc93dadad2b8593c3453518037d10095
lawrence <lawrence@outlook.com>
2025-06-02 04:52:16 +0800

    rerun and save result

---
+++ src/export_cashflow.py
@@ -0,0 +1,15 @@
+import os
+from sqlalchemy import create_engine
+import pandas as pd
+
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = "localhost"
+database = "zoo"
+
+engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{database}")
+
+cashflow = pd.read_sql("SELECT * FROM acc_cashflow",
+                       con=engine,
+                       dtype={'date':'datetime64[ns]', 'is_expense':'bool', 'is_group':'bool'})
+cashflow.to_feather('cashflow.feather')
